41f94eb6e79129b64088a3b5bca743d0dfadbd4d
luayg <84036260+luayg@users.noreply.github.com>
2026-01-27 04:14:38 +0300

    Update project

--- resources/views/admin/api/providers/file_services.blade.php
+++ resources/views/admin/api/providers/file_services.blade.php
@@ -1,66 +1,74 @@
-{{-- resources/views/admin/api/providers/file_services.blade.php --}}
-@extends('layouts.admin')
+@extends('layouts.app')
+
+@section('title')
+    File Services
+@endsection
 
 @section('content')
-<div class="card">
-  <div class="card-header"><h5 class="mb-0">{{ $provider->name }} | FILE services</h5></div>
+    @php
+        // خدمات تم إضافتها مسبقًا لهذا الـ provider
+        $existingRemoteIds = \App\Models\FileService::where('supplier_id', $provider->id)
+            ->pluck('remote_id')
+            ->toArray();
+    @endphp
 
-  <div class="card-body p-0">
-    <div class="table-responsive">
-      <table class="table table-sm table-striped mb-0">
-        <thead>
-          <tr>
-            <th>Group</th>
-            <th>Remote ID</th>
-            <th>Name</th>
-            <th>Credits</th>
-            <th>Time</th>
-            <th class="text-end">Action</th>
-          </tr>
-        </thead>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title mb-4">File Services - {{ $provider->name }}</h4>
 
-        <tbody>
-        @forelse($groups as $groupName => $items)
-          @foreach($items as $svc)
-            @php
-              $remoteId = $svc->remote_id;
-              $name     = $svc->name ?? '';
-              // ✅ الصحيح: السعر مخزن في price
-              $credit   = $svc->price ?? 0;
-              $time     = $svc->time ?? '';
-            @endphp
-            <tr>
-              <td>{{ $groupName }}</td>
-              <td><code>{{ $remoteId }}</code></td>
-              <td>{{ $name }}</td>
-              <td>{{ $credit }}</td>
-              <td>{{ $time }}</td>
-              <td class="text-end">
-                <button type="button" class="btn btn-success btn-sm"
-                  data-create-service
-                  data-service-type="file"
-                  data-provider-id="{{ $provider->id }}"
-                  data-remote-id="{{ $remoteId }}"
-                  data-group-name="{{ $groupName }}"
-                  data-name="{{ $name }}"
-                  data-credit="{{ $credit }}"
-                  data-time="{{ $time }}">
-                  Clone
-                </button>
-              </td>
-            </tr>
-          @endforeach
-        @empty
-          <tr><td colspan="6" class="text-center p-4">No data</td></tr>
-        @endforelse
-        </tbody>
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered">
+                            <thead>
+                            <tr>
+                                <th>Remote ID</th>
+                                <th>Name</th>
+                                <th>Credits</th>
+                                <th>Time</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($services as $service)
+                                @php
+                                    $remoteId = data_get($service, 'SERVICEID');
+                                    $isAdded = $remoteId !== null && in_array($remoteId, $existingRemoteIds, true);
+                                @endphp
 
-      </table>
+                                <tr>
+                                    <td>{{ $remoteId }}</td>
+                                    <td>{{ data_get($service, 'SERVICENAME') }}</td>
+                                    <td>{{ data_get($service, 'CREDIT') }}</td>
+                                    <td>{{ data_get($service, 'TIME') }}</td>
+                                    <td>
+                                        @if ($isAdded)
+                                            <button type="button" class="btn btn-outline-secondary btn-sm" disabled>
+                                                Added
+                                            </button>
+                                        @else
+                                            <button
+                                                type="button"
+                                                class="btn btn-primary btn-sm"
+                                                data-create-service="true"
+                                                data-service-type="file"
+                                                data-provider-name="{{ $provider->name }}"
+                                                data-service='@json($service)'
+                                            >
+                                                Clone
+                                            </button>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 
-<template id="serviceCreateTpl">
-  @include('admin.services.file._modal_create')
-</template>
+    @include('admin.api.providers.modals.services')
 @endsection
