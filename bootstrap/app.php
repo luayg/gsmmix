a688385a40434ac514fc40b205196dc83848176c
luayg <84036260+luayg@users.noreply.github.com>
2026-09-13 23:18:54 +0300

    security: register admin route protection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ProtectAdminRoutes;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(ProtectAdminRoutes::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
